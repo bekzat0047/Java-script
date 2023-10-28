3b6e4606538731fbf89b6ffd0e12c3e5942580ed
bekzat0047 <abdyraxmanov47@icloud.com>
2023-10-28 15:14:24 +0600

    avtan dil

---
+++ script.js
@@ -0,0 +1,19 @@
+const auto = {
+name:"Supra",
+year:2020,
+color: "red", 
+dimension: {
+    size: {
+windth: "1,99mm",
+length: "5,026"
+    }
+}
+
+ 
+}
+
+const avtan = {name,year,color} = auto;
+console.log(avtan);
+
+const characteristic = {windth , length } = auto;
+console.log(characteristic);
